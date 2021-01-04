61c78f700bd89582f3de75fdaf44c0df4069d21f
Pickra <picker10024@yahoo.com>
2021-01-04 17:00:59 -0500

    move store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from './features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
@@ -8,6 +8,7 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
