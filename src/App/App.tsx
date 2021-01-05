6bfbef8d512a62b65997ac7b9ffdea502b1bd393
apicker <ari.picker@oracle.com>
2021-01-04 19:13:39 -0500

    move app files to folder

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import logo from '../logo.svg';
+import { Counter } from '../features/counter/Counter';
 
 export const App = () => <div className="app">
   <header className="app-header">
