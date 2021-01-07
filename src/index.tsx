5acfdedbb0a987d1f7cbe27cf4c39752bccfe73f
apicker <ari.picker@oracle.com>
2021-01-07 12:16:48 -0500

    add redirect for 'users' route

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router } from "@reach/router";
+import { Redirect, Router } from "@reach/router";
 
 import './index.scss';
 import { Main, MainContent } from './Main/Main';
@@ -14,6 +14,7 @@ ReactDOM.render(
     <Provider store={store}>
       <Router basepath='/'>
         <Home path='/' />
+        <Redirect to={'1'} from={'users'} noThrow />
         <Main path='users'>
           <MainContent path=':page' />
         </Main>
