3c3b7908efd91b6c2029d92a3aa3b3264c05df58
apicker <ari.picker@oracle.com>
2021-01-07 10:53:22 -0500

    flesh out <Home /> a little

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,4 +1,8 @@
 import React, { FunctionComponent } from 'react';
-import { RouteComponentProps } from "@reach/router";
+import { Link, RouteComponentProps } from "@reach/router";
 
-export const Home: FunctionComponent<RouteComponentProps> = () => <div className='home'>poo</div>;
+export const Home: FunctionComponent<RouteComponentProps> = () => <section className='home'>
+    <h1>Welcome To The User Directory!</h1>
+    <p>We're all draons and unicorns here.</p>
+    <Link to='users/1'>Let's go see the users!</Link>
+</section>;
