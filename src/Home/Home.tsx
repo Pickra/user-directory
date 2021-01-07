0c03f379c432abf03877dcf0f42bc5a7b2bd5bc7
apicker <ari.picker@oracle.com>
2021-01-07 12:12:27 -0500

    minor link stylin tweaks

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import { Link, RouteComponentProps } from "@reach/router";
 
-export const Home: FunctionComponent<RouteComponentProps> = () => <section className='home'>
+export const Home: FunctionComponent<RouteComponentProps> = () => <main className='home'>
     <h1>Welcome To The User Directory!</h1>
-    <p>We're all draons and unicorns here.</p>
-    <Link to='users/1'>Let's go see the users!</Link>
-</section>;
+    <p>We're all dragons and unicorns here.</p>
+    <Link className='home__link' to='users/1'>Let's go see the users</Link>
+</main>;
