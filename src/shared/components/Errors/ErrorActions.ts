3928053b27c1b1c5e8438ca1318058060f9a3672
apicker <ari.picker@oracle.com>
2021-01-09 17:10:14 -0500

    added more error state

--- src/shared/components/Errors/ErrorActions.ts
+++ src/shared/components/Errors/ErrorActions.ts
@@ -1,12 +1,17 @@
 import { Action } from 'redux';
 
 export const ADD_USERS_ERROR = 'ADD_USER_ERROR';
-export type AddUsersErrorOptions = { fetch: string; };
-export type AddUsersErrorActionType = Action & AddUsersErrorOptions;
+export type AddUsersErrorActionType = Action & { fetch: string; };
 
-export const addUsersError = (options: AddUsersErrorOptions): any => ({
-    ...options,
+export const addUsersError = (options: string): any => ({
+    fetch: options,
     type: ADD_USERS_ERROR
 });
 
 export const REMOVE_USERS_ERROR = 'REMOVE_USER_ERROR';
+export type RemoveUsersDownloadErrorActionType = Action & { download: string; };
+
+export const addUsersDownloadError = (options: string): RemoveUsersDownloadErrorActionType => ({
+    download: options,
+    type: REMOVE_USERS_ERROR
+});
--- src/shared/components/Errors/ErrorReducer.ts
+++ src/shared/components/Errors/ErrorReducer.ts
@@ -1,15 +1,24 @@
 import { AppState, ErrorState } from '../../../store/store';
-import { AddUsersErrorActionType, ADD_USERS_ERROR } from './ErrorActions';
+import {
+    ADD_USERS_ERROR, REMOVE_USERS_ERROR,
+    AddUsersErrorActionType, RemoveUsersDownloadErrorActionType
+} from './ErrorActions';
 
 export const intialErrors: ErrorState = {
     userErrors: { fetch: '', download: '' }
 };
 
-export function errorReducer(prev = intialErrors, action: AddUsersErrorActionType) {
+type errorReducerAction = AddUsersErrorActionType | RemoveUsersDownloadErrorActionType;
+
+export function errorReducer(prev = intialErrors, action: errorReducerAction) {
     switch (action.type) {
         case ADD_USERS_ERROR:
-            const { fetch } = action;
+            const { fetch } = action as AddUsersErrorActionType;
             return { userErrors: { ...prev.userErrors, fetch }};
+
+        case REMOVE_USERS_ERROR:
+            const { download } = action as RemoveUsersDownloadErrorActionType;
+            return { userErrors: { ...prev.userErrors, download }};
 
         default:
             return prev;
